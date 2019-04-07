ed486fc8fe02192ea9bea5f89e7da37eb2f4d769
Amer-Alkharashgah <buildhorizon@gmail.com>
2019-04-07 14:29:31 +0300

    recursion

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,56 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
-  // your code goes here
-};
+
+
+ var stringifyJSON =function(obj) {
+
+var str = "" ;
+
+
+ ////// ///////// check if it is number or boolean or null
+
+	if(typeof obj==="number" || obj===null || obj===false || obj===true || typeof obj === "string")
+	{
+      str=str+obj;
+	}
+	     /////////////////
+
+	  else if(Array.isArray(obj)) /////// array check 
+	  {
+			str=str+"[";
+     
+	    for(var i=0 ; i<obj.length;i++) 
+	       {
+
+			str=str+stringifyJSON(obj[i]);	
+							
+				if(i!==obj.length-1)
+			        str+=",";
+			}
+
+			str = str + "]";
+		} 
+		 ////////// end of array check 
+      else if(typeof obj==="object")
+        {
+          str=str+"{";
+          for(var key in obj)
+          {
+          	str=str+key+":"+stringifyJSON(obj[key]);
+          	if(!(Object.keys(obj)[Object.keys(obj).length-1]===key))
+          		str+=",";
+          }
+          	
+		  str=str+"}" 
+        }
+      
+    return str ;
+	}
+
+     
+     
+
+
+
+   
